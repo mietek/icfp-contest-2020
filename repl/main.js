33f922289c9b9efdacda9cdcb2e065ff51724db9
Arek Flinik <arek.flinik@lekta.ai>
2020-07-19 01:24:45 +0200

    Hidden debug thingy, works as an easter egg as well

--- repl/main.js
+++ repl/main.js
@@ -143,6 +143,10 @@ function main() {
   const stdEnv = Env();
   handleInput(stdEnv, 'pwr2 = ap ap s ap ap c ap eq 0 1 ap ap b ap mul 2 ap ap b pwr2 ap add -1');
   handleInput(stdEnv, 'checkerboard = ap ap s ap ap b s ap ap c ap ap b c ap ap b ap c ap c ap ap s ap ap b s ap ap b ap b ap ap s i i lt eq ap ap s mul i nil ap ap s ap ap b s ap ap b ap b cons ap ap s ap ap b s ap ap b ap b cons ap c div ap c ap ap s ap ap b b ap ap c ap ap b b add neg ap ap b ap s mul div ap ap c ap ap b b checkerboard ap ap c add 2');
+  handleInput(stdEnv, 'cb1 = ap ap checkerboard 7 0');
+  handleInput(stdEnv, 'cb2 = ap ap checkerboard 7 1');
+  handleInput(stdEnv, 'cbs = ap ap cons cb1 ap ap cons cb2 nil');
+  handleInput(stdEnv, 'showmethemagic = ap multipledraw cbs'); // who doesn't like easter eggs
   document.getElementById('input').addEventListener('keydown', function(event) {
     handleKeyDown(stdEnv, event);
   });
