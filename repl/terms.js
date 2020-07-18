28a046521f18d5aa53c60d3e15b6a5472660bee4
Jan Dudek <jd@jandudek.com>
2020-07-17 17:03:55 -0700

    Add a mostly empty terms.js

---
+++ repl/terms.js
@@ -0,0 +1,91 @@
+
+// #13. Modulate
+// TODO
+
+// #14. Demodulate
+// TODO
+
+// #15. Send
+// TODO
+
+// #16. Negate
+// TODO
+
+// #17. Function Application
+// TODO
+
+// #18. S Combinator
+// TODO
+
+// #19. C Combinator
+// TODO
+
+// #20. B Combinator
+// TODO
+
+// #21. True (K Combinator)
+// TODO
+
+// #22. False
+// TODO
+
+// #23. Power of Two
+// TODO
+
+// #24. I Combinator
+// TODO
+
+// #25. Cons (or Pair)
+// TODO
+
+// #26. Car (First)
+// TODO
+
+// #27. Cdr (Tail)
+// TODO
+
+// #28. Nil (Empty List)
+// TODO
+
+// #29. Is Nil (Is Empty List)
+// TODO
+
+// #30. List Construction Syntax
+// TODO
+
+// #31. Vector
+// TODO
+
+// #32. Draw
+// TODO
+
+// #33. Checkerboard
+// TODO
+
+// #34. Multiple Draw
+// TODO
+
+// #35. Modulate List
+// TODO
+
+// #36. Send ( 0 )
+// TODO
+
+// #37. Is 0
+// TODO
+
+// #38. Interact
+// TODO
+
+// #39. Interaction Protocol
+// TODO
+
+// #40. Stateless Drawing Protocol
+// TODO
+
+// #41. Stateful Drawing Protocol
+// TODO
+
+// #42. Galaxy
+// TODO
+
