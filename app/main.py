28f5a1f22ab2c63e5273fea5c534226e1604528c
Alexander Juda <alexanderjuda@gmail.com>
2020-07-20 13:29:40 +0200

    Fix not using gravity for predicting enemy position for shooting

--- app/main.py
+++ app/main.py
@@ -759,7 +759,7 @@ def main():
                 cmds.append(_accelerate_command_dsl(ship['ship_id'], _make_acc_vector(*acceleration)))
 
             # shooting_coords = _predicted_position(enemy_ship_and_commands)
-            shooting_coords = _next_position(enemy_ship['position'], enemy_ship['velocity'])
+            shooting_coords = _predicted_trajectory(enemy_ship['position'], enemy_ship['velocity'], n=1)[0]
             shoot_cmd = _shoot_command_dsl(ship['ship_id'],
                                            Cons(shooting_coords[0],
                                                 shooting_coords[1]),
